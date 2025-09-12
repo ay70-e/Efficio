97a62fc33db24d96a843393ca67efabcdc9b1e60
ay70-e <ayalahebjaad@gmail.com>
2025-09-12 11:37:06 +0300

    login form

---
+++ Efficio/src/components/loginform.jsx
@@ -0,0 +1,67 @@
+import { useState } from "react";
+
+export default function Loginform() {
+  const [show, setShow] = useState(false);
+
+  return (
+
+    <div 
+ className="min-h-screen flex items-center justify-center bg-gray-100 relative">
+     
+     <button  onClick={() => setShow(!show)}
+         className="px-6 py-2 relative inline-flex items-end justify-center p-0.5 mb-2 
+         me-2 overflow-hidden text-sm font-medium text-[#7a8255] rounded-2xl
+          group bg-gradient-to-br from-[#ffd972] to-[#ffb7b2]
+           group hover:from-[#ffd972] group hover:to-[#b8c480] hover:text-white ">
+          Login
+          </button> 
+
+      {/* Login Form */}
+      <div  
+        className={`absolute p-10  bg-white rounded-2xl shadow-lg w-800 max-w-full transition-all duration-300 transform ${
+          show ? "opacity-100 scale-100 pointer-events-auto" : "opacity-0 scale-90 pointer-events-none"
+        }`}
+      > 
+      <div className="flex flex-col items-center gap-0.5 mb-4">
+        <h2 className="text-xl  text-[#080808] font-bold mb-4 justify-center rounded">Efficio</h2>
+        <p className=" text-[#08080886]">Login with your username and password</p>
+        </div>
+         <div className="flex flex-col gap-4 mb-4">
+         <div className="relative group w-full h-10">
+            <input
+              type="text"
+              placeholder="Username"
+              className="w-full h-full px-4 py-2 border-0 border-b-2 border-[#ffd972] text-sm font-medium
+              text-[#080808] bg-transparent z-10 relative focus:outline-none"
+            />
+            <span className="absolute bottom-0 left-0 w-full h-0 bg-[#ffd972] transition-all duration-1000 
+            group-focus-within:h-full  z-0"></span>
+          </div>
+          
+        <div className="relative group w-full h-10">
+            <input
+              type="password"
+              placeholder="Password"
+              className="w-full h-full px-4 py-2 border-0 border-b-2 border-[#ffd972] text-sm font-medium
+              text-[#7a8255] bg-transparent z-10 relative focus:outline-none"
+            />
+            <span className="absolute bottom-0 left-0 w-full h-0 bg-[#ffd972] transition-all duration-1000 
+            group-focus-within:h-full  z-0"></span>
+          </div>
+
+        <button
+            onClick={() => setShow(!show)}
+            className="relative w-full py-2 px-4 mb-2 me-2 overflow-hidden text-sm font-medium text-[#080808] rounded-2xl z-10
+                      bg-gradient-to-br from-[#b8c480] to-[#b8c480] transition-colors duration-300
+                      group hover:text-white"
+          >
+        <span className="absolute bottom-0 left-0 w-full h-0 bg-gradient-to-t from-[#ffd972] to-[#ffd972] z-0
+                            transition-all duration-700 ease-in-out group-hover:h-full rounded-2xl"></span>
+        <span className="relative z-10">Login</span>
+        </button>
+        </div>
+      </div>
+     
+    </div>
+  );
+}
