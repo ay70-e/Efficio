176434fe7675a59995502de7580d785999ca1c7f
Zain <zainalabdeenfalah@gmail.com>
2025-09-14 00:10:31 +0300

    shadcn add

--- Efficio/src/App.jsx
+++ Efficio/src/App.jsx
@@ -5,14 +5,22 @@ import Home from './pages/Home';
 import MangerBoard from './pages/MangerBoard';
 import ProjectsDashboard from './pages/ProjectsDashboard';
 import EmployeeBoard from './pages/EmployeeBoard';
+
+import { Button } from "@/components/ui/button"
+
+
 function App() {
  
 
   return (
     <>
-      <Navbar />
+      {/* <Navbar /> */}
+      <div className="p-4">
+        <Button variant="outline">Test Button - Click me!</Button>
+        <Button variant="default" className="ml-2">Primary Button</Button>
+        <Button variant="destructive" className="ml-2">Delete Button</Button>
+      </div>
       <Routes>
-        
        {/* <Route path='/' element={<Home/>}/> */}
        {/*  <Route path='/managerboard' element={<MangerBoard/>}/> */}
         <Route path='/projectsdashboard' element={<ProjectsDashboard/>}/>
